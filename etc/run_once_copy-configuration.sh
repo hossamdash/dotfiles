5f7ef4a8610c3749b9fa934e3a746cf01ccb535c
Hossam Mostafa <hossamdash2@gmail.com>
2021-08-11 09:35:12 +0200

    add remaining etc script

---
+++ etc/run_once_copy-configuration.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cp ./systemd /etc/systemd -rn
+cp ./doas.conf /etc -f
+cp ./fstab /etc -f
+cp ./hostname /etc -f
+cp ./hosts /etc -f
+cp ./locale.conf /etc -f
+cp ./makepkg.conf /etc -f
+cp ./pacman.conf /etc -f
+cp ./paru.conf /etc -f
+cp ./resolv.conf /etc -f
--- etc/snapper/run_once_configure-snapper.sh
+++ etc/snapper/run_once_configure-snapper.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 cp ./snapper /etc/conf.d/snapper -f
 cp ./home /etc/snapper/configs/home -f
 cp ./root /etc/snapper/configs/root -f
